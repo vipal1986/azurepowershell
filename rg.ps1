84511aef3ec95d9fda6a3f40a49330359c26c1b4
vipalg <vipalgujrathi@gmail.com>
2024-05-01 09:46:05 +0530

    added remove az group command

--- rg.ps1
+++ rg.ps1
@@ -1,13 +1,14 @@
 #connect to Azure
 
-Connect-AzAccount
+Connect-AzAccount -SubscriptionName "Visual Studio Professional Subscription"
+
 
 #create a resource group
 $resourcegroupname = "powershell-rg"
 $location = "East US"
 
-#New-AzResourceGroup -name $resourcegroupname -Location $location
+New-AzResourceGroup -name $resourcegroupname -Location $location
 
 #remove resource group
 
-Remove-AzResourceGroup -Name $resourcegroupname  -Confirm:$false 
+Remove-AzResourceGroup -Name $resourcegroupname  -Location $location -Confirm:$false 
